6e7c96ad8e461e39e16b9362913f68198a3cd3e8
Sittisukintaruk <fah78963214@gmail.com>
2021-05-14 13:29:11 +0700

    test server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.get('/', (req,res) => {
+    res.send('hello World')
+})
+
+app.listen(port,() => {
+    console.log(`listening at http://localhost:${port}`)})
+
+
